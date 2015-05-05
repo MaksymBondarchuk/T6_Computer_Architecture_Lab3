6cca36e207b9b413dbaf44e18c655ff29326638f
Maxim <maksim.bondarchuk@gmail.com>
2015-05-06 00:02:33 +0300

    Add, Update

--- python/test.py
+++ python/test.py
@@ -11,11 +11,15 @@ from dbxml import *
 # document = container.putDocument('lab5', '<lab><name>Lab5</name><state>Not completed</state></lab>', uc)
 
 # CREATE
-def add():
+def add(name, about, status):
     mgr = XmlManager()
     container = mgr.openContainer("labs.dbxml")
     uc = mgr.createUpdateContext()
-    container.putDocument('Lab 3', '<lab><about>CRUD</about><state>We\'re working on</state></lab>', uc)
+    try:
+        container.putDocument(name, '<lab><about>%s</about><state>%s</state></lab>' % (about, status), uc)
+    except:
+        return 'already exist with this name'
+    return 'OK'
 
 
 # READ
@@ -31,24 +35,36 @@ def read():
 
 
 # UPDATE
-def update():
+def update(name, field, new_value):
     mgr = XmlManager()
     uc = mgr.createUpdateContext()
     container = mgr.openContainer("labs.dbxml")
 
-    qc = mgr.createQueryContext()
-    # qc.setNamespace("labs2", "labs.dbxml")
+    document = container.getDocument(name)
+    name = document.getName()
+    content = document.getContent()
 
-    results = mgr.query("collection('labs.dbxml')//*[status='None']", qc)
-    results.reset()
-    for value in results:
-        document = value.asDocument()
-        print document.getName(), "=", value.asString()
+    if field == 'name':
+        try:
+            container.deleteDocument(name, uc)
+            container.putDocument(new_value, content, uc)
+        except:
+            container.putDocument(name, content, uc)
+            return 'already exist with this name'
+        return 'OK'
+
+    import xml.etree.ElementTree as ET
+    tree = ET.fromstring(content)
+    tree.find(field).text = new_value
+
+    content_new = ET.tostring(tree, encoding="utf8", method="html")
+
+    container.deleteDocument(name, uc)
+    container.putDocument(name, content_new, uc)
 
 
 # DELETE
 def delete():
-    import os
     mgr = XmlManager()
     container = mgr.openContainer("labs.dbxml")
     uc = mgr.createUpdateContext()
@@ -61,8 +77,11 @@ def delete():
 
 print('Before')
 read()
-add()
+
+print('\n' + add('Lab 2', 'Web workers', 'Completed'))
 # delete()
+# print('\n' + update('Lab 31', 'name', 'Lab 1'))
+
 print('\nAfter')
 read()
-# update()
+
